3719ac674a9a2c4135e44f541f4970a5f4988be8
SaniyaParab <saniyaparab12@gmail.com>
2026-09-13 22:50:29 +0530

    BH-2 Implement login functionality

---
+++ src/main/java/com/bookhub/App.java
@@ -0,0 +1,12 @@
+package com.bookhub;
+
+public class App {
+
+    public String getMessage() {
+        return "Welcome to BookHub!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to BookHub!");
+    }
+}
---
+++ src/test/java/com/bookhub/AppTest.java
@@ -0,0 +1,14 @@
+package com.bookhub;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppTest {
+
+    @Test
+    void testMessage() {
+        App app = new App();
+
+        assertEquals("Welcome to BookHub!", app.getMessage());
+    }
+}
